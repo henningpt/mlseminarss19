bddbf6bfc4c001d0a5607988344064561e41a2ec
henningpt <henning.ptaszyk@udo.edu>
2019-06-12 12:59:46 +0200

    add clean_data.py

---
+++ clean_dataset.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+
+from pandas.plotting import scatter_matrix
+from matplotlib import pyplot as plt
+
+
+# test
+print("test1")
+df = pd.DataFrame(put_dataset_here/export.csv)
+df.head(100)
+
+print("test2")
